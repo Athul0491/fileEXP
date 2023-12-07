b4dffa2a4758988e728d0c13d05cd83ef192d49c
Athul0491 <65664185+Athul0491@users.noreply.github.com>
2023-12-08 02:29:43 +0530

    bugFix: fixed the GetDisks function

--- app.go
+++ app.go
@@ -12,12 +12,12 @@ import (
 	"strings"
 )
 
-type Drive struct{
-	name string
-	used_gb int
-	total_gb int
-	letter string
-}
+// type Drive struct{
+// 	name string
+// 	used_gb int
+// 	total_gb int
+// 	letter string
+// }
 // App struct
 type App struct {
 	ctx context.Context
@@ -71,7 +71,7 @@ func (a *App) OpenDirectory(path string) ([]string, error){
 	return allFiles, nil
 }
 
-func (a *App) GetDisks() ([]Drive, error) {
+func (a *App) GetDisks() ([]map[string]interface{}, error) {
 	cmd := exec.Command("wmic", "logicaldisk", "get", "deviceid,freespace,size")
 	output, err := cmd.Output()
 	if err != nil {
@@ -79,22 +79,24 @@ func (a *App) GetDisks() ([]Drive, error) {
 	}
 
 	lines := strings.Split(string(output), "\n")
-	var diskSpaces []Drive
+	diskSpaces  := []map[string]interface{}{}
 
 	for _, line := range lines[1:] {
 		fields := strings.Fields(line)
 		if len(fields) == 3 {
-			used_gb ,_ := strconv.Atoi(fields[1])
+			used_gb, _ := strconv.Atoi(fields[1])
 			total_gb, _:= strconv.Atoi(fields[2])
-			diskSpace := Drive{
-				name:   fields[0],
-				used_gb:  used_gb,
-				total_gb: total_gb,
-				letter: fields[0][0:1],
-			}
+			// diskSpace := Drive{
+			// 	name:   fields[0],
+			// 	used_gb:  used_gb,
+			// 	total_gb: total_gb,
+			// 	// letter: fields[0][0:1],
+			// }
+			diskSpace := map[string]interface{}{"name": fields[0], "used_gb": used_gb,"total_gb":total_gb,"letter":fields[0][0:1]}
 			diskSpaces = append(diskSpaces, diskSpace)
+			
 		}
 	}
-
+	fmt.Println(diskSpaces)
 	return diskSpaces, nil
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,91 +1,17 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import { OpenDirectory } from "../wailsjs/go/main/App";
-import { GetDisks } from "../wailsjs/go/main/App";
-import { PiArrowUUpLeftBold } from "react-icons/pi";
-// interface Disk {
-//   name: string;
-//   used_gb: number;
-//   total_gb: number;
-// }
-const App = () => {
-  const [disks, setDisks] = useState([]);
+import { useEffect } from "react";
+import { GetDisks } from "../../frontend/wailsjs/go/main/App.js";
+
+function App() {
   useEffect(() => {
     async function getData() {
       const disks = await GetDisks();
-      setDisks(disks);
+      console.log({ disks });
     }
+
     getData().catch(console.error);
   }, []);
-  console.log(disks);
-  const [currentDirectory, setCurrentDirectory] = useState("D:/");
-  const [previousDirectory, setPreviousDirectory] = useState("");
-  const [directoryContents, setDirectoryContents] = useState(["D:/"]);
 
-  const showDirectoryContents = async (directory) => {
-    try {
-      const content = await OpenDirectory(directory);
-      setDirectoryContents(content);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleClick = (dir) => {
-    console.log("handle click");
-    console.log(dir);
-    setPreviousDirectory(currentDirectory);
-    setCurrentDirectory(dir);
-  };
-
-  const handleBackClick = () => {
-    if (previousDirectory !== "") {
-      setCurrentDirectory(previousDirectory);
-      setPreviousDirectory(getParentDirectory(previousDirectory));
-    }
-  };
-
-  useEffect(() => {
-    console.log(currentDirectory);
-    showDirectoryContents(currentDirectory);
-  }, [currentDirectory]);
-
-  const getParentDirectory = (filePath) => {
-    const segments = filePath.split("/");
-    segments.pop();
-    const parentDirectory = segments.join("/");
-    return parentDirectory + "/";
-  };
-
-  return (
-    <div id="App">
-      <div>
-        <PiArrowUUpLeftBold
-          className="backButton"
-          onClick={handleBackClick}
-          size="2em"
-        />
-      </div>
-      <div
-        id="directoryContents"
-        className="directoryContents max-h-100 overflow-y-auto"
-      >
-        <h3>
-          {directoryContents.length > 0 &&
-            directoryContents.map((dir, index) => (
-              <div
-                onClick={() => handleClick(dir)}
-                key={index}
-                id={dir}
-                className={`directoryContent directoryContent${index}`}
-              >
-                <p>{dir}</p>
-              </div>
-            ))}
-        </h3>
-      </div>
-    </div>
-  );
-};
+  return <div></div>;
+}
 
 export default App;
